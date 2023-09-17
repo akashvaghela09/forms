70f58c9e3d62d812ae51b866a70fb769b14d6eb9
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-09-18 03:33:41 +0530

    Update homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <AppRoutes />
     </div>
   );
 }
