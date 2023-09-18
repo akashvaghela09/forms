280917417bc5643798278303c6f2d7dbdaaece4d
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-09-19 02:17:03 +0530

    Add App logo

---
+++ client/src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import { SiGoogleforms  } from "react-icons/si";
+
+const Logo = () => {
+    return (
+        <div className="flex justify-center items-center gap-2 p-4 w-fit">
+            <SiGoogleforms className="text-indigo-500 text-4xl"/>
+            <h1 className="text-4xl font-bold text-indigo-500">Forms</h1>
+        </div>
+    )
+}
+
+export default Logo;
