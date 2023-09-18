99855bced29bf755019fa4e9a394e1b2c78c0211
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-09-18 05:42:23 +0530

    Add verify endpoint

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -60,9 +60,37 @@ const test = (req, res) => {
     res.json({ message: 'Authenticated successfully', user: req.user });
 };
 
+const verify = (req, res) => {
+    // Get the token from the headers
+    const authHeader = req.headers['authorization'];
+
+    if (!authHeader) {
+        return res.status(401).json({ message: 'No token provided', success: false });
+    }
+
+    // Split the Bearer token to get the actual token
+    const token = authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided', success: false });
+    }
+
+    try {
+        // Verify the token with the secret key
+        const decoded = jwt.verify(token, "your_jwt_secret_key");
+
+        // If verification is successful, the token is valid
+        return res.status(200).json({ message: 'JWT is valid', user: decoded, success: true });
+    } catch (error) {
+        // If verification fails, the token is invalid
+        return res.status(401).json({ message: 'JWT is invalid', error: error.message, success: false });
+    }
+}
+
 module.exports = {
     register,
     login,
     authenticate,
     test,
+    verify
 };
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -5,5 +5,6 @@ const userController = require('../controllers/userController.js');
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.get('/test', userController.authenticate, userController.test);
+router.get('/verify', userController.verify);
 
 module.exports = router;
