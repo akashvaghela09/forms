160a4122a8839be4df3088bc68d54e6a91113b99
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-09-20 00:24:04 +0530

    Add chakra ui

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </Router>
 );
