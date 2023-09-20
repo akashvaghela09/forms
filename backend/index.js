8d18716fd659d088a4c8743e2e4213ff8d72ad74
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-09-21 00:11:56 +0530

    Fix cors issue

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors({
+    origin: [
+        'http://localhost:3000', 
+        // 'https://your-production-url.com'
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 const userRoutes = require('./src/routes/userRoutes');
 const formRoutes = require('./src/routes/formRoutes');
 
