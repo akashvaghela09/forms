094783b56e2ccf8ed9bc1d7a77004926a4f5868d
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-09-21 06:26:41 +0530

    CSS fixes

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -2,7 +2,7 @@ import { SiGoogleforms  } from "react-icons/si";
 
 const Logo = () => {
     return (
-        <div className="flex justify-center items-center gap-2 p-4 w-fit">
+        <div className="flex justify-center items-baseline gap-2 p-4 w-fit">
             <SiGoogleforms className="text-indigo-500 text-4xl"/>
             <h1 className="text-4xl font-bold text-indigo-500">Forms</h1>
         </div>
